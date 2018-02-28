f4bcac7d417c65d8bcce13d325bba1b63fb8815c
BertZZ <bertie.wooles@hotmail.co.uk>
2018-02-28 20:33:13 +0000

    /accounts route presents all accounts as JSON

--- app/app.rb
+++ app/app.rb
@@ -1,9 +1,26 @@
 require 'sinatra/base'
+require 'rest-client'
+require 'json'
+
 
 class App < Sinatra::Base
 
   get '/' do
     "Project set up Basics are working"
+  end
+
+  get '/accounts' do
+    content_type :json
+    response = RestClient.get('https://mvf-devtest-s3api.s3-eu-west-1.amazonaws.com/a4a06bb0-3fbe-40bd-9db2-f68354ba742f.json')
+    json = JSON.parse(response)
+    accounts = json["accounts"]
+    accounts.to_json
+
+  end
+
+  get '/example' do
+    content_type :json
+    { :key1 => 'value1', :key2 => 'value2' }.to_json
   end
 
 end

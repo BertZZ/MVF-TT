aaec03db80006191207479989edbe4d2df002300
BertZZ <bertie.wooles@hotmail.co.uk>
2018-03-02 13:57:15 +0000

    Adds test for sorting by balance for User story 3.3 but doesn't quite find a solution

--- test.rb
+++ test.rb
@@ -4,17 +4,43 @@ require 'json'
 
 def parse_Json
   response = RestClient.get('https://mvf-devtest-s3api.s3-eu-west-1.amazonaws.com/a4a06bb0-3fbe-40bd-9db2-f68354ba742f.json')
-  h = JSON.parse(response)
-  user = h["accounts"].find.each do {|h1| h1["lastname"]=="Irwin"}
+  json = JSON.parse(response)
+  return json
+end
+
+
+def method
+  balances = []
+  output = parse_Json
+  users = output["accounts"]
+    users.each do |user|
+      if user["balance"].is_a? String
+        commaless = user["balance"].gsub(/\,/,"")
+        balances << commaless.to_i
+      else
+        balances << user["balance"].to_i
+      end
+    end
+  sorted = balances.sort.reverse
+end
+
+
+
+def sort_obj
+  users_array = []
+  output = parse_Json
+  users = output["accounts"]
+  users.each do |user|
+    if user["balance"].is_a? String
+      commaless = user["balance"].gsub!(/\,/,"")
+        if user["balance"].to_i > 1000 && user["balance"].to_i < 2000
+          users_array << user
+        end
+      end
   end
-  puts user
+  puts users_array
+  sorted = users_array.sort_by! { |k,balance| [:balance].to_i }
+  puts sorted
 end
 
-
-def sort
-  a = [4,7,8,145,42000,1,-6,72]
-  a.sort!
-  puts a
-end
-
-parse_Json
+sort_obj
